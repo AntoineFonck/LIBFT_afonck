6e56663a450b3a0abc225341acab851e8d9369f0
Antoine Fonck <afonck@MacBook-Pro-de-Antoine.local>
2019-05-07 13:40:10 +0200

    converting int to unsigned int makes the function work

--- LIBFT/ft_printf/ft_itoa_base.c
+++ LIBFT/ft_printf/ft_itoa_base.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../libft.h"
+#include <unistd.h>
 
-char *itoa_base(int value, int base)
+//char *itoa_base(int value, int base)
+void	ft_itoaprint_base(unsigned int value, int base)
 {
-    static  char buf[50];
+    //static  char buf[50];
+	char buf[50];
     char    *ptr;
     int     num;
-    //int	    ascii_offset;
+    int	    ascii_offset;
 
     ptr = &buf[49];
     *ptr = '\0';
@@ -17,23 +21,27 @@ char *itoa_base(int value, int base)
         *--ptr = '0' + (value % base);
     while (value != 0)
     {
-	//ascii_offset = 0;
-	//if (value % base > 9)
-	//	ascii_offset = 39;
-        //*--ptr = '0' + ((value % base) + ascii_offset);
-	*--ptr = "0123456789abcdef"[value % base];
+	ascii_offset = 0;
+	if (value % base > 9)
+		ascii_offset = 39;
+        *--ptr = '0' + ((value % base) + ascii_offset);
+	//*--ptr = "0123456789abcdef"[value % base];
         value /= base;
     }
     if (num < 0 && base == 10)
         *--ptr = '-';
-    return (ptr);
+    //return (ptr);
+    write(1, ptr, ft_strlen(ptr));
 }
 
 int main(int argc, char **argv)
 {
 	if (argc == 3)
 	{
-		printf("%s\n", itoa_base(atoi(argv[1]), atoi(argv[2])));
+		//printf("%s\n", itoa_base(atoi(argv[1]), atoi(argv[2])));
+		ft_itoaprint_base(ft_atoi(argv[1]), ft_atoi(argv[2]));
+		printf("\nreal printf hex = %x\n", ft_atoi(argv[1]));
+		printf("\nreal printf octal = %o\n", ft_atoi(argv[1]));
 	}
 	return (0);
 }
