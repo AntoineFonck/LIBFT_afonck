70353d5df6a5ae8dd6313d1afa3813fb35062112
Antoine FONCK <afonck@e1r3p5.42.fr>
2019-05-08 14:49:00 +0200

    [CRITICAL] implementation of flags handling -> structure method

--- LIBFT/ft_printf/ft_printf.c
+++ LIBFT/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 14:51:24 by afonck            #+#    #+#             */
-/*   Updated: 2019/05/07 18:52:08 by afonck           ###   ########.fr       */
+/*   Updated: 2019/05/08 14:48:10 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,47 @@ int	do_function(char c, int *fd, va_list args)
 	return (0);
 }
 
-int ft_vprintf(int fd, const char *fmt, va_list args)
+t_flags *init_flags()
+{
+	t_flags *flags;
+
+	if ((flags = (t_flags *)malloc(sizeof(t_flags))) == NULL)
+		return (NULL);
+	flags->hashtag = 0;
+	flags->minus = 0;
+	flags->plus = 0;
+	flags->space = 0;
+	flags->zero = 0;
+	flags->field_width = 0;
+	flags->precision = 0;
+	return (flags);
+}
+
+int  is_flag(char c)
+{
+	if (c == '#' || c == '-' || c == '+' || c == ' ' || c == '0')
+		return (1);
+	else
+		return (0);
+}
+
+void activate_flags(t_flags *flags, char c)
+{
+	if (c == '#')
+		flags->hashtag = 1;
+	else if (c == '-')
+		flags->minus = 1;
+	else if (c == '+')
+		flags->plus = 1;
+	else if (c == ' ')
+		flags->space = 1;
+	else if (c == '0')
+		flags->zero = 1;
+	else
+		return ;
+}
+
+int ft_vprintf(int fd, const char *fmt, va_list args, t_flags *flags)
 {
 	int tmp_int;
 	char tmp_char;
@@ -93,6 +133,12 @@ int ft_vprintf(int fd, const char *fmt, va_list args)
 		if (*fmt == '%')
 		{
 			fmt++;
+			while (is_flag(*fmt))
+			{
+				activate_flags(flags, *fmt);
+				printf("[DEBUG] flag loop activated, flag working? = %d\n", flags->hashtag);
+				fmt++;
+			}
 			total_len += do_function(*fmt, &fd, args);
 			fmt++;
 		}
@@ -110,13 +156,27 @@ int ft_printf(const char *fmt, ...)
 {
 	int done;
 	va_list args;
+	t_flags *flags;
 
+	if ((flags = init_flags()) == NULL)
+		return (-1);
 	va_start(args, fmt);
-	done = ft_vprintf(1, fmt, args);
+	done = ft_vprintf(1, fmt, args, flags);
 	va_end(args);
+	ft_memdel((void **)&flags);
 	return (done);
 }
 
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		ft_printf(argv[1]);
+	}
+	return (0);
+}
+
+/*
 int main(int argc, char *argv[])
 {
 	if (argc == 2)
@@ -134,3 +194,4 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+*/
--- LIBFT/ft_printf/ft_printf.h
+++ LIBFT/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 14:51:35 by afonck            #+#    #+#             */
-/*   Updated: 2019/05/07 18:31:16 by afonck           ###   ########.fr       */
+/*   Updated: 2019/05/08 13:44:18 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 # define TRUE 1
 
 void	ft_uitoaprint_base(unsigned int value, int base, int fd);
+
+typedef	struct s_flags
+{
+	int hashtag;
+	int minus;
+	int plus;
+	int space;
+	int zero;
+	int field_width;
+	int precision;
+}				t_flags;
 
 typedef struct s_converter 
 {
