a826e5d2049e2c4466c39b9c16f417cc56610031
Antoine FONCK <antoinefonck98@gmail.com>
2019-06-24 15:37:07 +0200

    ftoa a bit corrected

--- LIBFT/ft_printf/ft_ftoa.c
+++ LIBFT/ft_printf/ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: sluetzen <sluetzen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 22:21:42 by afonck            #+#    #+#             */
-/*   Updated: 2019/06/24 15:14:53 by sluetzen         ###   ########.fr       */
+/*   Updated: 2019/06/24 15:38:15 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,24 +38,19 @@ static int	handle_decimal(double n, double fpart, int afterpoint, int fd, int mo
 		ft_putchar_fd('.', fd);
 		length++;
 		fpart = fpart * ft_pow(10, afterpoint);
-		printf("FPART after pow = %f\n", fpart);
 		fpart = (n >= 0 ? fpart + 0.5 : fpart - 0.5);
 		fpart = ft_absolute(fpart);
-		//morezero = afterpoint - ft_floatlen(fpart, afterpoint);//- ft_nbrlen((unsigned long)fpart);
 		morezero = afterpoint - ft_nbrlen((long)fpart);
-		printf("afterpoint = %d et nbrlen fpart = %d and the fpart too = %ld\n", afterpoint, ft_nbrlen((long)fpart), (long)fpart);
-		printf("morezero = %d and fpart = %ld\n", morezero, (long)fpart);
 		if (morezero > 0)
 		{
-			printf("HEREEEEEEEEEEEEEEE0");
 			pad_zero(morezero, fd);
 			length += morezero;
 		}
-		printf("HEREEEEEEEEEEEEEEE2");
-		length += ft_nbrlen(ft_absolute((long)fpart));
-		ft_uitoaprint_base(ft_absolute((unsigned long)fpart), 10, fd);
+		//length += ft_nbrlen(ft_absolute((long)fpart));
+		//ft_uitoaprint_base(ft_absolute((unsigned long)fpart), 10, fd);
+		length += ft_nbrlen((long)fpart);
+		ft_uitoaprint_base((unsigned long)fpart, 10, fd);
 	}
-	printf("length of deci = %d\n", length);
 	return (length);
 }
 
@@ -77,9 +72,7 @@ int			ft_ftoa(double n, int afterpoint, int fd)
 			ipart += 1;
 	}
 	ft_putnbr_fd(ipart, fd);
-	printf("FPART = %f\n", fpart);
 	len += handle_decimal(n, fpart, afterpoint, fd, morezero);
-	printf("full float len = %d\n", len);
 	return (len);
 }
 
@@ -95,7 +88,6 @@ int		ft_floatlen(double n, int afterpoint)
 	fpart = fpart * ft_pow(10, afterpoint);
 	fpart = (n >= 0 ? fpart + 0.5 : fpart - 0.5);
 	len += ft_nbrlen(ipart);
-	printf("[DEBUG] IN FLOAT LEN, len = %d after += IPART\n", len);
 	if (afterpoint != 0)
 	{
 		if (ft_absolute((long)fpart) != 0)
@@ -103,6 +95,5 @@ int		ft_floatlen(double n, int afterpoint)
 		else
 			len += afterpoint + 1;
 	}
-	printf("[DEBUG] IN FLOAT LEN, len = %d\n", len);
 	return (len);
 }
