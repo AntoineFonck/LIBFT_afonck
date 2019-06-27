76e94ee294a5363add335ec8fca96cd3c30c78f6
Antoine FONCK <antoinefonck98@gmail.com>
2019-06-27 16:36:47 +0200

    libft in printf directory + testing makefile

---
+++ LIBFT/ft_printf/libft/ft_putnbr_fd.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afonck <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/13 15:59:15 by afonck            #+#    #+#             */
+/*   Updated: 2018/11/13 16:11:06 by afonck           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdint.h>
+#include <unistd.h>
+
+static void	putdis(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void		ft_putnbr_fd(intmax_t n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(-21474, fd);
+		ft_putnbr_fd(83648, fd);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			putdis(('-'), fd);
+			n = -n;
+		}
+		if (n >= 10)
+		{
+			ft_putnbr_fd((n / 10), fd);
+			ft_putnbr_fd((n % 10), fd);
+		}
+		else
+		{
+			putdis(('0' + n), fd);
+		}
+	}
+}
