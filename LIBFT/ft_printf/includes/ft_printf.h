6820cd453a4d3ee073ff6b4daa4e480c6e31a84c
Antoine FONCK <antoinefonck98@gmail.com>
2019-07-14 09:14:12 +0200

    header final norme ok

--- LIBFT/ft_printf/includes/ft_printf.h
+++ LIBFT/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sluetzen <sluetzen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 14:51:35 by afonck            #+#    #+#             */
-/*   Updated: 2019/07/08 15:42:55 by sluetzen         ###   ########.fr       */
+/*   Updated: 2019/07/14 09:13:42 by afonck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 # include <stdint.h>
 # include <wchar.h>
 
-# define HASHTAG  1   /* 0000 0001 */
-# define MINUS  2   /* 0000 0010 */
-# define PLUS  4   /* 0000 0100 */
-# define SPACE  8   /* 0000 1000 */
-# define ZERO  16  /* 0001 0000 */
-# define HH  32  /* 0010 0000 */
-# define H  64  /* 0100 0000 */
-# define L  128 /* 1000 0000 */
-# define LL  256 /* 1 0000 0000 */
-# define PREC  512 /* 10 0000 0000 */
-# define BIGL  1024 /* 100 0000 0000 */
+# define HASHTAG  1
+# define MINUS  2
+# define PLUS  4
+# define SPACE  8
+# define ZERO  16
+# define HH  32
+# define H  64
+# define L  128
+# define LL  256
+# define PREC  512
+# define BIGL  1024
 
 # define HASH_FLAG flags->state & HASHTAG
 # define MIN_FLAG flags->state & MINUS
@@ -51,20 +51,23 @@
 
 # define NBFORMATS 14
 
-int		ft_printf(const char *fmt, ...); /*__attribute__((format(printf,1,2)));*/
+int				ft_printf(const char *fmt, ...);
+/*
+**	__attribute__((format(printf,1,2)));
+*/
 
 typedef	struct	s_flags
 {
-	int state;
-	int field_width;
-	int precision;
-	int color;
+	int	state;
+	int	field_width;
+	int	precision;
+	int	color;
 }				t_flags;
 
 typedef struct	s_converter
 {
-	char format;
-	int (*fun_ptr)(va_list, int, t_flags *);
+	char		format;
+	int			(*fun_ptr)(va_list args, int fd, t_flags *flags);
 }				t_converter;
 
 /*
@@ -79,98 +82,101 @@ typedef struct	s_converter
 ** check functions -------------------------------------
 */
 
-void	check_flags(const char **fmt, t_flags *flags);
+void			check_flags(const char **fmt, t_flags *flags);
 
-void	check_field_width(const char **fmt, t_flags *flags);
+void			check_field_width(const char **fmt, t_flags *flags);
 
-void	check_precision(const char **fmt, t_flags *flags);
+void			check_precision(const char **fmt, t_flags *flags);
 
-void	check_lmod(const char **fmt, t_flags *flags);
+void			check_lmod(const char **fmt, t_flags *flags);
 
-void	check_color(const char **fmt, t_flags *flags);
+void			check_color(const char **fmt, t_flags *flags);
 
 /*
 ** flags functions -------------------------------------
 */
 
-t_flags	*init_flags(void);
+t_flags			*init_flags(void);
 
-int	is_flag(char c);
+int				is_flag(char c);
 
-void	activate_flags(t_flags *flags, char c);
+void			activate_flags(t_flags *flags, char c);
 
-int	is_activated(t_flags *flags);
+int				is_activated(t_flags *flags);
 
-void	flush_flags(t_flags *flags);
+void			flush_flags(t_flags *flags);
 
 /*
 ** convert functions ------------------------------------
 */
 
-int	convert_float(va_list args, int fd, t_flags *flags);
+int				convert_float(va_list args, int fd, t_flags *flags);
 
-int	convert_percent(va_list args, int fd, t_flags *flags);
+int				convert_percent(va_list args, int fd, t_flags *flags);
 
-int	convert_string(va_list args, int fd, t_flags *flags);
+int				convert_string(va_list args, int fd, t_flags *flags);
 
-int	convert_wstring(va_list args, int fd, t_flags *flags);
+int				convert_wstring(va_list args, int fd, t_flags *flags);
 
-int	convert_char(va_list args, int fd, t_flags *flags);
+int				convert_char(va_list args, int fd, t_flags *flags);
 
-int	convert_wchar(va_list args, int fd, t_flags *flags);
+int				convert_wchar(va_list args, int fd, t_flags *flags);
 
-int	convert_cap_hex(va_list args, int fd, t_flags *flags);
+int				convert_cap_hex(va_list args, int fd, t_flags *flags);
 
-int	convert_hex(va_list args, int fd, t_flags *flags);
+int				convert_hex(va_list args, int fd, t_flags *flags);
 
-int	convert_pointer(va_list args, int fd, t_flags *flags);
+int				convert_pointer(va_list args, int fd, t_flags *flags);
 
-int	convert_int(va_list args, int fd, t_flags *flags);
+int				convert_int(va_list args, int fd, t_flags *flags);
 
-int	convert_uint(va_list args, int fd, t_flags *flags);
+int				convert_uint(va_list args, int fd, t_flags *flags);
 
-int	convert_oct(va_list args, int fd, t_flags *flags);
+int				convert_oct(va_list args, int fd, t_flags *flags);
 
-int	convert_bin(va_list args, int fd, t_flags *flags);
+int				convert_bin(va_list args, int fd, t_flags *flags);
 
 /*
 ** pad functions ----------------------------------------
 */
 
-int	pad_float(double number, t_flags *flags, int fd);
+int				pad_float(double number, t_flags *flags, int fd);
 
-int	pad_int(intmax_t number, t_flags *flags, int fd);
+int				pad_int(intmax_t number, t_flags *flags, int fd);
 
-int	pad_str(int number, t_flags *flags, int fd);
+int				pad_str(int number, t_flags *flags, int fd);
 
-int	pad_uint(uintmax_t number, t_flags *flags, int fd);
+int				pad_uint(uintmax_t number, t_flags *flags, int fd);
 
-int	pad_hex_prec_mincap(int hexlen, t_flags *flags, int fd, uintmax_t hex);
+int				pad_hex_prec_mincap(int hexlen, t_flags *flags, int fd, \
+				uintmax_t hex);
 
-int	pad_int_prec(intmax_t number, t_flags *flags, int fd);
+int				pad_int_prec(intmax_t number, t_flags *flags, int fd);
 
-int	pad_uint_prec(uintmax_t number, t_flags *flags, int fd);
+int				pad_uint_prec(uintmax_t number, t_flags *flags, int fd);
 
-int	pad_oct(int octlen, t_flags *flags, int fd);
+int				pad_oct(int octlen, t_flags *flags, int fd);
 
-int	pad_oct_prec(int octlen, t_flags *flags, int fd);
+int				pad_oct_prec(int octlen, t_flags *flags, int fd);
 
-int	pad_float_prec(double number, int preclen, t_flags *flags, int fd);
+int				pad_float_prec(double number, int preclen, t_flags *flags, \
+				int fd);
 
 /*
 ** utils functions -------------------------------------
 */
 
-void	flags_spec(t_flags *flags, int nbpad, int fd, intmax_t number);
+void			flags_spec(t_flags *flags, int nbpad, int fd, intmax_t number);
 
-int	special_hexzero(int fd, t_flags *flags);
+int				special_hexzero(int fd, t_flags *flags);
 
-int	special_convert_hex(uintmax_t hex, int fd, t_flags *flags, char letter);
+int				special_convert_hex(uintmax_t hex, int fd, t_flags *flags, \
+				char letter);
 
-void	choose_color(int fd, int color);
+void			choose_color(int fd, int color);
 
-void	pad_zero(int nbzero, int fd);
+void			pad_zero(int nbzero, int fd);
 
-void	pad_space(int nbpad, int fd);
+void			pad_space(int nbpad, int fd);
 
 #endif
